f35a49a61bc3f33046fbc72a1ac31bfee7c14338
brendandebeasi <b@debea.si>
2015-08-02 17:54:15 -0700

    Added bullettttttts

--- js/game.js
+++ js/game.js
@@ -65,21 +65,96 @@ var Platform = function (game, key,x,y) {
 Platform.prototype = Object.create(Phaser.Sprite.prototype);
 Platform.prototype.constructor = Platform;
 
-//Bullet.prototype.fire = function (x, y, angle, speed, gx, gy) {
-//
-//    gx = gx || 0;
-//    gy = gy || 0;
-//
-//    this.reset(x, y);
-//    this.scale.set(1);
-//
-//    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
-//
-//    this.angle = angle;
-//
-//    this.body.gravity.set(gx, gy);
-//
-//};
+var Weapon = {};
+
+////////////////////////////////////////////////////
+//  A single bullet is fired in front of the ship //
+////////////////////////////////////////////////////
+
+Weapon.SingleBullet = function (game, source) {
+
+    Phaser.Group.call(this, game, game.world, 'Single Bullet', false, true, Phaser.Physics.ARCADE);
+
+    this.nextFire = 0;
+    this.bulletSpeed = 600;
+    this.fireRate = 100;
+
+    for (var i = 0; i < 64; i++)
+    {
+        this.add(new Bullet(game, 'bullet'), true);
+    }
+
+    return this;
+
+};
+
+Weapon.SingleBullet.prototype = Object.create(Phaser.Group.prototype);
+Weapon.SingleBullet.prototype.constructor = Weapon.SingleBullet;
+
+Weapon.SingleBullet.prototype.fire = function (source) {
+
+    var angle = 270;
+    if(source.body.velocity.x>0) var angle=0;
+    else if(source.body.velocity.x<0) angle = 180;
+
+    if (this.game.time.time < this.nextFire) { return; }
+
+    var x = source.x + 10;
+    var y = source.y + 10;
+
+    this.getFirstExists(false).fire(x, y, angle, this.bulletSpeed, 0, 0, source);
+
+    this.nextFire = this.game.time.time + this.fireRate;
+
+};
+
+var Bullet = function (game, key) {
+
+    Phaser.Sprite.call(this, game, 0, 0, key);
+
+    //this.anchor.set(0.5);
+
+    this.checkWorldBounds = true;
+    this.outOfBoundsKill = true;
+    this.exists = false;
+
+    this.tracking = false;
+    this.scaleSpeed = 0;
+
+};
+
+Bullet.prototype = Object.create(Phaser.Sprite.prototype);
+Bullet.prototype.constructor = Bullet;
+
+Bullet.prototype.fire = function (x, y, angle, speed, gx, gy, source) {
+    gx = gx || 0;
+    gy = gy || 0;
+
+    this.reset(x, y);
+    this.scale.set(1);
+
+    this.game.physics.arcade.velocityFromAngle(angle, speed, this.body.velocity);
+
+    this.angle = angle;
+
+    this.body.gravity.set(gx, gy);
+
+};
+
+Bullet.prototype.update = function () {
+
+    if (this.tracking)
+    {
+        this.rotation = Math.atan2(this.body.velocity.y, this.body.velocity.x);
+    }
+
+    if (this.scaleSpeed > 0)
+    {
+        this.scale.x += this.scaleSpeed;
+        this.scale.y += this.scaleSpeed;
+    }
+
+};
 
 
 
@@ -105,6 +180,7 @@ PhaserGame.prototype = {
         this.load.image('star', 'img/star.png');
         this.load.image('turret', 'img/turret.png');
         this.load.spritesheet('dude', 'img/player.png', 32, 32);
+        this.load.spritesheet('bullet', 'img/bullet.png', 6, 6);
     },
 
     create: function () {
@@ -117,6 +193,9 @@ PhaserGame.prototype = {
         //  Player physics properties. Give the little guy a slight bounce.
         this.player.body.gravity.y = 900;
         this.player.body.collideWorldBounds = true;
+
+        this.weapons.push(new Weapon.SingleBullet(this.game));
+        this.currentWeapon = 0;
 
         //create ground
         this.platforms = new Ground(this.game);
@@ -151,10 +230,10 @@ PhaserGame.prototype = {
         {
             this.player.body.velocity.y = -600;
         }
-        //if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
-        //{
-        //    this.weapons[this.currentWeapon].fire(this.player);
-        //}
+        if (this.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR))
+        {
+            this.weapons[this.currentWeapon].fire(this.player);
+        }
 
     }
 
